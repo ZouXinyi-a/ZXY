0c168c9b53133a1083ef0321ec615a89327fe284
ZouXinyi-a <1749883446.qq.com@example.com>
2020-05-25 17:08:49 +0800

    第八周实验2第3小题

---
+++ 8实验2（3）/8实验2（3）/8实验2（3）Doc.h
@@ -0,0 +1,49 @@
+﻿
+// 8实验2（3）Doc.h: CMy8实验23Doc 类的接口
+//
+
+
+#pragma once
+
+
+class CMy8实验23Doc : public CDocument
+{
+protected: // 仅从序列化创建
+	CMy8实验23Doc() noexcept;
+	DECLARE_DYNCREATE(CMy8实验23Doc)
+
+// 特性
+public:
+
+// 操作
+public:
+	CRect cr;
+	bool m;
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~CMy8实验23Doc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
---
+++ 8实验2（3）/8实验2（3）/8实验2（3）View.cpp
@@ -0,0 +1,106 @@
+﻿
+// 8实验2（3）View.cpp: CMy8实验23View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "8实验2（3）.h"
+#endif
+
+#include "8实验2（3）Doc.h"
+#include "8实验2（3）View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy8实验23View
+
+IMPLEMENT_DYNCREATE(CMy8实验23View, CView)
+
+BEGIN_MESSAGE_MAP(CMy8实验23View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CMy8实验23View 构造/析构
+
+CMy8实验23View::CMy8实验23View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMy8实验23View::~CMy8实验23View()
+{
+}
+
+BOOL CMy8实验23View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMy8实验23View 绘图
+
+void CMy8实验23View::OnDraw(CDC* pDC)
+{
+	CMy8实验23Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	pDC->Ellipse(pDoc->cr);
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMy8实验23View 诊断
+
+#ifdef _DEBUG
+void CMy8实验23View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMy8实验23View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMy8实验23Doc* CMy8实验23View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMy8实验23Doc)));
+	return (CMy8实验23Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMy8实验23View 消息处理程序
+
+
+void CMy8实验23View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CMy8实验23Doc* pDoc = GetDocument();
+	CClientDC dc(this);
+	pDoc->m = true;
+	CPen *oldpen = NULL;
+	CPen pen(PS_DASH, 1, RGB(0, 0, 255));
+	GetDC()->SelectStockObject(NULL_BRUSH);
+	CBrush *oldbrush = NULL;
+	CBrush *brush = CBrush::FromHandle((HBRUSH)GetStockObject(NULL_BRUSH));
+	oldbrush = dc.SelectObject(brush);
+	if (point.x > pDoc->cr.left && point.x < pDoc->cr.right) {
+		if (point.y > pDoc->cr.top && point.y < pDoc->cr.bottom) {
+			oldpen = dc.SelectObject(&pen);
+			dc.Rectangle(pDoc->cr);
+			dc.SelectObject(oldpen);
+		}
+	}
+	CView::OnLButtonDown(nFlags, point);
+}
